b17a4fcce4c70c75be787c30fa31c7263efbc8f7
Gregory Nelson Esguerra <gregorye@ggsinc.com>
2024-10-05 00:10:00 +0800

    Add project files.

---
+++ BlazorQuickGrid/Components/Pages/Home.Razor.cs
@@ -0,0 +1,46 @@
+﻿using BlazorQuickGrid.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.QuickGrid;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorQuickGrid.Components.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        IDbContextFactory<WideWorldImportersContext>? contextFactory { get; set; }
+
+        private PaginationState pagination = new PaginationState { ItemsPerPage = 10 };
+        private string Title = "Wide World Importers - Customers";
+        private IQueryable<Customer>? itemsQueryable;
+        private string nameFilter = string.Empty;
+
+        private IQueryable<Customer>? FilteredCustomers
+        {
+            get
+            {
+                IQueryable<Customer>? Customers = itemsQueryable;
+
+                if (!string.IsNullOrEmpty(nameFilter))
+                    Customers = itemsQueryable?.Where(c => c.CustomerName.Contains(nameFilter, StringComparison.CurrentCultureIgnoreCase));
+
+                if (Customers != null)
+                    return Customers.AsQueryable();
+
+                return null;
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var context = contextFactory.CreateDbContext();
+            var customers = await context.Customers
+                        .Include(x => x.CustomerCategory)
+                        .Include(y => y.DeliveryCity)
+                        .Include(z => z.PrimaryContactPerson)
+                        .ToListAsync();
+
+            itemsQueryable = customers.AsQueryable();
+        }
+    }
+}
---
+++ BlazorQuickGrid/Program.cs
@@ -0,0 +1,42 @@
+using BlazorQuickGrid.Components;
+using BlazorQuickGrid.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorQuickGrid
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            builder.Services.AddDbContextFactory<WideWorldImportersContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
